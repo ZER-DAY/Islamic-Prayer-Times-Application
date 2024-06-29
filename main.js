058aa9bd51632c0c02b167b3b5a86555713867a8
ZER-DAY <118398476+ZER-DAY@users.noreply.github.com>
2024-06-29 22:48:04 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,110 @@
+
+let cities = [
+    {
+        arabicName: "غزة",
+        name: "PS-GAZ"
+    },
+    {
+        arabicName: "اليمن",
+        name: "YE"
+    },
+    {
+        arabicName: "مكة",
+        name: "SA"
+    },
+    {
+        arabicName: "القاهرة",
+        name: "EG-CAI"
+    },
+    {
+        arabicName: "الرباط",
+        name: "MA-RAB"
+    },
+    {
+        arabicName: "الدوحة",
+        name: "QA-DOH"
+    },
+    {
+        arabicName: "عمان",
+        name: "JO-AMM"
+    },
+    {
+        arabicName: "بغداد",
+        name: "IQ-BGW"
+    },
+    {
+        arabicName: "دمشق",
+        name: "SY-DAM"
+    },
+    {
+        arabicName: "الرياض",
+        name: "SA-RYD"
+    }
+];
+
+for(let city of cities ){
+    const content = `
+    <option value="${city.arabicName}">${city.arabicName}</option>
+    `
+    document.getElementById("cityselcted").innerHTML += content
+}
+
+
+    document.getElementById("cityselcted").addEventListener("change", function() {
+      document.getElementById("city-name").innerHTML =this.value
+        let cityName = ""
+        for(let city of cities ){
+        if(city.arabicName === this.value ){
+            cityName = city.name
+        }
+    }
+    console.log("sd")
+    getPrayersOfCity(cityName)
+    });
+
+
+
+function getPrayersOfCity(cityName){
+    let params = {
+        country: "PS",
+        city: cityName //"PS-GAZ",
+    }
+    
+    axios.get('http://api.aladhan.com/v1/timingsByCity/:date', {
+        params: params
+      })
+      .then(function (response) {
+          // let fajer = 
+        // document.getElementById("fajer-time").innerHTML= timings.Fajr
+        const timings = response.data.data.timings
+        fillTimeforpray("fajer-time",timings.Fajr)
+        fillTimeforpray("shroq-time",timings.Sunrise)
+        fillTimeforpray("doher-time",timings.Dhuhr)
+        fillTimeforpray("aser-time",timings.Asr)
+        fillTimeforpray("maghreb-time",timings.Maghrib)
+        fillTimeforpray("asha-time",timings.Isha)
+    
+    
+        const readableDate = response.data.data.date.readable;
+        const weekDay = response.data.data.date.hijri.weekday.ar;
+        
+        const date =   weekDay + "" + readableDate
+        document.getElementById("time-now").innerHTML = date
+        // طباعة التاريخ القابل للقراءة للتحقق منه
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });  
+}
+
+
+
+
+function fillTimeforpray(id ,time){
+    document.getElementById(id).innerHTML= time
+}
+
+
